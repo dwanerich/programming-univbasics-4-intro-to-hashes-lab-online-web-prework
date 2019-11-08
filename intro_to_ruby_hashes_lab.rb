7f867f88ac73679e0a22564248e5c60ce56deb92
dwane richards <dwanerich@gmail.com>
2019-11-08 02:39:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,65 +1,57 @@
 def new_hash
-  new_hash = {
+  
+  dwane = {
     
   }
 end
 
+
+
 def my_hash
-  my_hash = {
-    :name => "dwane"
+  people = {
+    dwane: 'leader'
   }
 end
+
+
+
+
+
 
 def pioneer
   
   pioneer = {
-    :name => "Grace Hopper"
+    :name => 'Grace Hopper'
   }
-  
+
 end
 
 def id_generator
-  id_generator = {
+  hash = {
     :id => 3
   }
+ 
 end
-
-
-
 
 def my_hash_creator(key, value)
    
-  hash = {
-    key => value
-  }
-
+ hash = {
+   key => value 
+ }
 
 end
 
-
-
-
-
 def read_from_hash(hash, key)
     
-    hash[key]
+ hash[key]
 
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
-  else
-    hash[key] = 1
-  end
-
-  hash
-
+      if hash[key]
+        hash[key] += 1
+      else
+        hash[key] = 1
+      end
+      hash
 end
-
-
-  # return the correct value using the hash and key parameters read_from_hash instruction
-  
-  # given a hash an a key as parameters, return an updated hash update_counting_hash instructions
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
